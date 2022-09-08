af740435e4dc7f8f9e8d078591089bd728545b53
Hanif <amrhanif15@gmail.com>
2022-09-08 22:10:40 +0700

    finished the header and footer part

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,10 @@ navToggle.addEventListener("click", () => {
     navToggle.classList.toggle("opened")
     navMobile.classList.toggle("opened")
 })
+
+const scrollToTopBtn = document.querySelector("footer .scroll-to-top")
+scrollToTopBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    window.scrollTo(0, 0)
+})
