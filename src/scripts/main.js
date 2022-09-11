5d24aa34b3f5314e7d133795bd4e37d6685233c8
Hanif <amrhanif15@gmail.com>
2022-09-11 19:47:01 +0700

    finish sign page, and applied loading animation to all pages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,34 @@
+// mobile navigation
 const navToggle = document.querySelector("header .nav-toggle")
 const navMobile = document.querySelector(".nav-mobile")
+if(navToggle && navMobile)
+{
+    navToggle.addEventListener("click", () => {
+        navToggle.classList.toggle("opened")
+        navMobile.classList.toggle("opened")
+    })
+}
 
-navToggle.addEventListener("click", () => {
-    navToggle.classList.toggle("opened")
-    navMobile.classList.toggle("opened")
-})
-
+// scroll to top 
 const scrollToTopBtn = document.querySelector("footer .scroll-to-top")
-scrollToTopBtn.addEventListener('click', (e) => {
-    e.preventDefault()
+if(scrollToTopBtn)
+{
+    scrollToTopBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+    
+        window.scrollTo(0, 0)
+    })
+}
 
-    window.scrollTo(0, 0)
+// loading animation
+const stopLoadingAnimation = () => {
+    const body = document.querySelector("body")
+    const loaderContainer = document.querySelector(".loaderContainer")
+
+    body.classList.remove("loading")
+    loaderContainer.classList.add("hide-animation")
+}
+
+window.addEventListener("load", () => {
+    stopLoadingAnimation();
 })
