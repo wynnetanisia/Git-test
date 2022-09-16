3318e0d36669e4af8deefe3cb266667aaf5ce1b3
Hanif <amrhanif15@gmail.com>
2022-09-17 00:28:02 +0700

    finished new blog and educative page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -32,3 +32,30 @@ const stopLoadingAnimation = () => {
 window.addEventListener("load", () => {
     stopLoadingAnimation();
 })
+
+// Animate On Scroll functionality 
+const aosObjects = document.querySelectorAll(".aos-base")
+if(aosObjects)
+{
+    // offset for customize on what height I want to make the animation active
+    const elInView = (el, offset) => {
+        let elementTop = el.getBoundingClientRect().top
+        
+        // return true of false
+        return (
+            elementTop <= ((window.innerHeight || document.documentElement.clientHeight) - offset)
+        )
+    }
+
+    const addAnimation = (el) => el.classList.add("scrolled")
+
+    const removeAnimation = (el) => el.classList.remove("scrolled")
+
+    const runAnimation = () => {
+        aosObjects.forEach(obj => {
+            if(elInView(obj, 50)) addAnimation(obj)
+        })
+    }
+
+    window.addEventListener("scroll", () => runAnimation())
+}
